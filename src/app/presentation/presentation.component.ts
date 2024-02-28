b4976dd097178d7f796a8132caa909e5180bdc2a
JulienBoileau <julien.boilleau.01@gmail.com>
2024-02-29 00:29:10 +0100

    Ajout partenaires et clients

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
-  styleUrls: ['./presentation.component.css']
+  styleUrls: ['./presentation.component.css', './carousel.css']
 })
 export class PresentationComponent {
 
